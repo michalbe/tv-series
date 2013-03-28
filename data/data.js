768a30119e5b43c4d65dcb35bb7153886fb6f89f
Michal Budzynski <michal@virtualdesign.pl>
2013-03-28 01:57:38 +0100

    House of Cards fix

--- data/data.js
+++ data/data.js
@@ -151,8 +151,8 @@ var series = [{
 }, {
   "title": "House of Cards",
   "imdb": "http://www.imdb.com/title/tt1856010",
-  "episodes": "9",
-  "totalEpisodes": 14,
+  "episodes": 10,
+  "totalEpisodes": 13,
   "episodeLength": 60,
   "stillWatching": 1
 }, {
