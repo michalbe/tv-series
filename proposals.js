142a1e541f62fc9474bf3249cc7b9509ffc70626
Michal Budzynski <michal@virtualdesign.pl>
2013-03-28 01:38:34 +0100

    to-consider list added

---
+++ proposals.js
@@ -0,0 +1,181 @@
+// Add your proposal to the list, but please, 
+// remmeber about alphabetical order. If your show is
+// already on the list, just put your github account
+// in the 'votes' part. Thanks!
+
+var toConsider = [{
+  "title": "Band of Brothers",
+  "imdb": "http://www.imdb.com/title/tt0185906",
+  "episodes": "10",
+  "episodeLength": 60,
+  "stillWatching": 0,
+  "votes": ["tobeytailor"]
+}, {
+  "title": "Chuck",
+  "imdb": "http://www.imdb.com/title/tt0934814",
+  "episodes": "91",
+  "episodeLength": 43,
+  "stillWatching": 1,
+  "votes": ["kamilogorek"]
+}, {
+  "title": "Cowboy Bebop",
+  "imdb": "http://www.imdb.com/title/tt0213338/",
+  "episodes": "26",
+  "episodeLength": 25,
+  "stillWatching": 0,
+  "votes": ["datrio"]
+}, {
+  "title": "Homeland",
+  "imdb": "http://www.imdb.com/tt1796960",
+  "episodes": "24",
+  "episodeLength": 60,
+  "stillWatching": 1,
+  "votes": ["tobeytailor", "shorang"]
+}, {
+  "title": "Louie",
+  "imdb": "http://www.imdb.com/title/tt1492966",
+  "episodes": "39",
+  "episodeLength": 23,
+  "stillWatching": 1,
+  "votes": ["kamilogorek"]
+}, {
+  "title": "Mad Men",
+  "imdb": "http://www.imdb.com/title/tt0804503",
+  "episodes": "65",
+  "episodeLength": 45,
+  "stillWatching": 1,
+  "votes": ["kamilogorek", "datrio"]
+}, {
+  "title": "Modern Family",
+  "imdb": "http://www.imdb.com/title/tt1442437/",
+  "episodes": "86",
+  "episodeLength": 30,
+  "stillWatching": 1,
+  "votes": ["afronski"]
+}, {
+  "title": "My Little Pony: Friendship is Magic",
+  "imdb": "http://www.imdb.com/title/tt1751105/",
+  "episodes": "65",
+  "episodeLength": 22,
+  "stillWatching": 1,
+  "votes": ["dos1"]
+}, {
+  "title": "My Name Is Earl",
+  "imdb": "http://www.imdb.com/title/tt0460091",
+  "episodes": "96",
+  "episodeLength": 30,
+  "stillWatching": 0,
+  "votes": ["kamilogorek"]
+}, {
+  "title": "Numb3rs",
+  "imdb": "http://www.imdb.com/title/tt0433309/",
+  "episodes": "118",
+  "episodeLength": 43,
+  "stillWatching": 0,
+  "votes": ["afronski"]
+}, {
+  "title": "Oz",
+  "imdb": "http://www.imdb.com/title/tt0118421",
+  "episodes": "56",
+  "episodeLength": 55,
+  "stillWatching": 0,
+  "votes": ["kamilogorek"]
+}, {
+  "title": "Parks and Recreations",
+  "imdb": "http://www.imdb.com/title/tt1266020",
+  "episodes": "83",
+  "episodeLength": 21,
+  "stillWatching": 1,
+  "votes": ["kamilogorek"]
+}, {
+  "title": "Person of Interest",
+  "imdb": "http://www.imdb.com/title/tt1839578",
+  "episodes": "39",
+  "episodeLength": 43,
+  "stillWatching": 0,
+  "votes": ["shorang"]
+}, {
+  "title": "Pitbull",
+  "imdb": "http://www.imdb.com/title/tt1096980",
+  "episodes": "31",
+  "episodeLength": 45,
+  "stillWatching": 0,
+  "votes": ["kamilogorek", "afronski"]
+}, {
+  "title": "Sons of Anarchy",
+  "imdb": "http://www.imdb.com/title/tt1124373",
+  "episodes": "56",
+  "episodeLength": 45,
+  "stillWatching": 1,
+  "votes": ["kamilogorek", "tobeytailor"]
+}, {
+  "title": "Spartacus",
+  "imdb": "http://www.imdb.com/title/tt1442449",
+  "episodes": "33",
+  "episodeLength": 60,
+  "stillWatching": 1,
+  "votes": ["kamilogorek", "datrio", "afronski", "tobeytailor"]
+}, {
+  "title": "The Following",
+  "imdb": "http://www.imdb.com/title/tt2071645",
+  "episodes": "5",
+  "episodeLength": 43,
+  "stillWatching": 0,
+  "votes": ["shorang"]
+}, {
+  "title": "The Newsroom",
+  "imdb": "http://www.imdb.com/tt1870479",
+  "episodes": "10",
+  "episodeLength": 60,
+  "stillWatching": 1,
+  "votes": ["tobeytailor"]
+}, {
+  "title": "The Office",
+  "imdb": "http://www.imdb.com/title/tt0386676",
+  "episodes": "179",
+  "episodeLength": 22,
+  "stillWatching": 1,
+  "votes": ["kamilogorek"]
+}, {
+  "title": "The Pacific",
+  "imdb": "http://www.imdb.com/tt0374463",
+  "episodes": "10",
+  "episodeLength": 60,
+  "stillWatching": 0,
+  "votes": ["tobeytailor"]
+}, {
+  "title": "The Shield",
+  "imdb": "http://www.imdb.com/tt0286486",
+  "episodes": "88",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["tobeytailor"]
+}, {
+  "title": "The Sopranos",
+  "imdb": "http://www.imdb.com/tt0141842",
+  "episodes": "86",
+  "episodeLength": 55,
+  "stillWatching": 0,
+  "votes": ["tobeytailor"]
+}, {
+  "title": "The Walking Dead",
+  "imdb": "http://www.imdb.com/title/tt1520211/",
+  "episodes": "28",
+  "episodeLength": 44,
+  "stillWatching": 1,
+  "votes": ["datrio", "afronski", "tobeytailor", "shorang"]
+}, {
+  "title": "The West Wing",
+  "imdb": "http://www.imdb.com/title/tt0200276/",
+  "episodes": "156",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["datrio"]
+}, {
+  "title": "Wilfred",
+  "imdb": "http://www.imdb.com/title/tt1703925",
+  "episodes": "26",
+  "episodeLength": 30,
+  "stillWatching": 1,
+  "votes": ["kamilogorek"]
+}]
