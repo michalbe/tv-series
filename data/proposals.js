03e01599f13c778d8f2edd41c7cf58d359329725
Michal Budzynski <michal@virtualdesign.pl>
2013-04-16 14:00:00 +0100

    da vincis deamons in proposals

--- data/proposals.js
+++ data/proposals.js
@@ -25,6 +25,12 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["datrio"]
 }, {
+  "title": "Da Vinci's Demons",
+  "imdb": "http://www.imdb.com/title/tt2094262/",
+  "episodes": "2",
+  "episodeLength": 60,
+  "stillWatching": 1
+},  {
   "title": "Homeland",
   "imdb": "http://www.imdb.com/tt1796960",
   "episodes": "24",
