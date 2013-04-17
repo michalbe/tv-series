14ebe454692c55f805920cd8fc14d5d86920903f
Michal Budzynski <michal@virtualdesign.pl>
2013-04-17 14:00:00 +0100

    game of thrones

--- data/data.js
+++ data/data.js
@@ -132,7 +132,7 @@ var series = [{
 }, {
   "title": "Game of Thrones",
   "imdb": "http://www.imdb.com/title/tt0944947",
-  "episodes": 23,
+  "episodes": 24,
   "episodeLength": 60,
   "stillWatching": 1
 },
