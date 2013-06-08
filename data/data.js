cf0af40f510122f5880e88b859e9f3e2729466d2
Michal Budzynski <michal@virtualdesign.pl>
2013-06-08 20:07:00 +0100

    sipnsons

--- data/data.js
+++ data/data.js
@@ -288,7 +288,7 @@ var series = [{
 }, {
   "title": "Simpsons",
   "imdb": "http://www.imdb.com/title/tt0096697/",
-  "episodes": 22,
+  "episodes": 23,
   "totalEpisodes": "523",
   "episodeLength": 24,
   "stillWatching": 1
