8552075668a219af9be1ef2da82eaed0ee69f8ac
Michal Budzynski <michal@virtualdesign.pl>
2013-06-10 20:07:00 +0100

    revenge

--- data/data.js
+++ data/data.js
@@ -257,7 +257,7 @@ var series = [{
 }, {
   "title": "Revenge",
   "imdb": "http://www.imdb.com/title/tt1837642/",
-  "episodes": 11,
+  "episodes": 13,
   "episodeLength": 44,
   "stillWatching": 1
 }, {
