b6244a53213435ba06b639b5422112956ce62719
Michal Budzynski <michal@virtualdesign.pl>
2013-07-04 15:03:23 +0200

    dexter

--- data/data.js
+++ data/data.js
@@ -108,7 +108,7 @@ var series = [{
 },{
   "title": "Dexter",
   "imdb": "http://www.imdb.com/title/tt0773262/",
-  "episodes": "84",
+  "episodes": 85,
   "episodeLength": 50,
   "stillWatching": 1
 },{
