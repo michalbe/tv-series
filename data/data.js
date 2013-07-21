72eceb86aeb758629b1661ccd3352e67e6e0d694
Michal Budzynski <michal@virtualdesign.pl>
2013-07-22 00:16:31 +0200

    louie

--- data/data.js
+++ data/data.js
@@ -225,7 +225,7 @@ var series = [{
 }, {
   "title": "Louie",
   "imdb": "http://www.imdb.com/title/tt1492966",
-  "episodes": 29,
+  "episodes": 30,
   "episodeLength": 23,
   "stillWatching": 1
 }, {
