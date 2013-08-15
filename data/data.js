53f33a5532d79f58d2920108003b71f41b9954c8
Michal Budzynski <michal@virtualdesign.pl>
2013-08-15 21:07:00 +0100

    joey

--- data/data.js
+++ data/data.js
@@ -206,7 +206,7 @@ var series = [{
 },{
   "title": "Joey",
   "imdb": "http://www.imdb.com/title/tt0375355/",
-  "episodes": 10,
+  "episodes": 11,
   "episodeLength": 24,
   "stillWatching": 1
 }, {
