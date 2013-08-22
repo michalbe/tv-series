59995d90ec49941117d82928e10ec83f08097cd8
Michal Budzynski <michal@virtualdesign.pl>
2013-08-22 21:07:00 +0100

    f and b

--- data/data.js
+++ data/data.js
@@ -132,7 +132,7 @@ var series = [{
 }, {
   "title": "Franklin & Bash",
   "imdb": "http://www.imdb.com/title/tt1600199/",
-  "episodes": 18,
+  "episodes": 19,
   "episodeLength": 42,
   "stillWatching": 1
 }, {
