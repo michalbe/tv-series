af7d569ded8bfbec7858a1693b0fadaf5e63fe73
Michal Budzynski <michal@virtualdesign.pl>
2013-09-30 16:35:29 +0200

    bb

--- data/data.js
+++ data/data.js
@@ -57,7 +57,7 @@ var series = [{
 }, {
   "title": "Breaking Bad",
   "imdb": "http://www.imdb.com/title/tt0903747/",
-  "episodes": 61,
+  "episodes": 62,
   "episodeLength": 47,
   "stillWatching": 1
 }, {
