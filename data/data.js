cc03a6acbcc6a53dd74234956f68c4165fdbcb56
Michal Budzynski <michal@virtualdesign.pl>
2013-10-03 21:07:00 +0100

    elemetary

--- data/data.js
+++ data/data.js
@@ -114,7 +114,7 @@ var series = [{
 },{
   "title": "Elementary",
   "imdb": "http://www.imdb.com/title/tt2191671/",
-  "episodes": 24,
+  "episodes": 25,
   "episodeLength": 44,
   "stillWatching": 1
 }, {
