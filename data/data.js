15ad56bf224bbebeb404f5486bc0536c4684547d
Michal Budzynski <michal@virtualdesign.pl>
2013-10-11 21:07:00 +0100

    t70s

--- data/data.js
+++ data/data.js
@@ -337,7 +337,7 @@ var series = [{
 }, {
   "title": "That '70s Show",
   "imdb": "http://www.imdb.com/title/tt0165598/",
-  "episodes": 1156,
+  "episodes": 116,
   "totalEpisodes": 200,
   "episodeLength": 22,
   "stillWatching": 1
