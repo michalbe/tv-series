2aa72cb15e42a1e8ee3a26e238fbc6507c0591ae
Michal Budzynski <michal@virtualdesign.pl>
2013-10-14 02:05:38 +0200

    2.5m

--- data/data.js
+++ data/data.js
@@ -344,7 +344,7 @@ var series = [{
 },{
   "title": "Two and a half men",
   "imdb": "http://www.imdb.com/title/tt0369179",
-  "episodes": 226,
+  "episodes": 227,
   "episodeLength": 21,
   "stillWatching": 1
 }, {
