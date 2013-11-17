0e73cd59c1eb207b8afb343b9b0245c149ce5185
Michal Budzynski <michal@virtualdesign.pl>
2013-11-17 14:00:00 +0100

    the mentalist

--- data/data.js
+++ data/data.js
@@ -256,7 +256,7 @@ var series = [{
 }, {
   "title": "Mentalist",
   "imdb": "http://www.imdb.com/title/tt1196946",
-  "episodes": 122,
+  "episodes": 123,
   "episodeLength": 42,
   "stillWatching": 1
 }, {
