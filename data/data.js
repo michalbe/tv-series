09c7fd9c7897ad8b9fcdba4e79bb8377ac6f4122
Michal Budzynski <michal@virtualdesign.pl>
2013-11-24 14:00:00 +0100

    friends

--- data/data.js
+++ data/data.js
@@ -144,7 +144,7 @@ var series = [{
 },{
   "title": "Friends",
   "imdb": "http://www.imdb.com/title/tt0108778/",
-  "episodes": 1,
+  "episodes": 3,
   "totalEpisodes": 239,
   "episodeLength": 22,
   "stillWatching": 1
