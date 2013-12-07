ea0170767d7347448d77aa88adb47d3be00ef2ee
Michal Budzynski <michal@virtualdesign.pl>
2013-12-07 21:07:00 +0100

    update

--- data/data.js
+++ data/data.js
@@ -14,7 +14,7 @@ var series = [{
 }, {
   "title": "American Dad",
   "imdb": "http://www.imdb.com/title/tt0397306",
-  "episodes": 155,
+  "episodes": 157,
   "episodeLength": 21,
   "stillWatching": 1
 }, {
