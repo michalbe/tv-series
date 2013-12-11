81b1ecd04b0b32bc6cae060ced6ccfaf68b835bf
Michal Budzynski <michal@virtualdesign.pl>
2013-12-11 21:07:00 +0100

    update

--- data/data.js
+++ data/data.js
@@ -45,7 +45,7 @@ var series = [{
 }, {
   "title": "Big Bang Theory",
   "imdb": "http://www.imdb.com/title/tt0898266/",
-  "episodes": 146,
+  "episodes": 147,
   "episodeLength": 22,
   "stillWatching": 1
 }, {
