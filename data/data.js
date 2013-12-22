ef79b30968e1363f48451d6c407f800251a9ba8c
Michal Budzynski <michal@virtualdesign.pl>
2013-12-22 21:07:00 +0100

    update

--- data/data.js
+++ data/data.js
@@ -120,7 +120,7 @@ var series = [{
 }, {
   "title": "Family Guy",
   "imdb": "http://www.imdb.com/title/tt0182576/",
-  "episodes": 210,
+  "episodes": 219,
   "episodeLength": 22,
   "stillWatching": 1
 }, {
