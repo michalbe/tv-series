9492ed674c5b65396af38a2dceddbe0c0c49ad0d
Michal Budzynski <michal@virtualdesign.pl>
2014-01-11 21:07:00 +0100

    update

--- data/data.js
+++ data/data.js
@@ -33,7 +33,7 @@ var series = [{
 }, {
   "title": "Arrow",
   "imdb": "http://www.imdb.com/title/tt2193021/",
-  "episodes": 32,
+  "episodes": 33,
   "episodeLength": 44,
   "stillWatching": 1
 }, {
