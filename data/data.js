606c004e9c939876dd625d62eaa1d5aa940a8b9b
Michal Budzynski <michal@virtualdesign.pl>
2014-01-12 21:07:00 +0100

    update

--- data/data.js
+++ data/data.js
@@ -189,7 +189,7 @@ var series = [{
 }, {
   "title": "How I met your mother",
   "imdb": "http://www.imdb.com/title/tt0460649/",
-  "episodes": 197,
+  "episodes": 198,
   "episodeLength": 22,
   "stillWatching": 1
 }, {
