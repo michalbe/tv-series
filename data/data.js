bbcc4f21c527adf45fca08d4425c5542f813427d
Michal Budzynski <michal@virtualdesign.pl>
2014-01-27 01:56:14 +0100

    [TV Shows] Shameless US

--- data/data.js
+++ data/data.js
@@ -312,7 +312,7 @@ var series = [{
 }, {
   "title": "Shameless US",
   "imdb": "http://www.imdb.com/title/tt1586680",
-  "episodes": 35,
+  "episodes": 36,
   "episodeLength": 55,
   "stillWatching": 1
 }, {
