6912a25ae36accaa33fe597800faf1cfea744a41
Michal Budzynski <michal@virtualdesign.pl>
2014-02-21 21:07:00 +0100

    [TV Shows] Dr Who

--- data/data.js
+++ data/data.js
@@ -114,7 +114,7 @@ var series = [{
 },{
   "title": "Doctor Who",
   "imdb": "http://www.imdb.com/title/tt0436992/",
-  "episodes": 42,
+  "episodes": 43,
   "episodeLength": 45,
   "stillWatching": 1
 },{
