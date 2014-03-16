91f40973968f03fc40a6f5406fc96a2401cf37ba
Michal Budzynski <michal@virtualdesign.pl>
2014-03-16 21:01:03 +0100

    [Tv Show] 2.5 men

--- data/data.js
+++ data/data.js
@@ -402,10 +402,10 @@ var series = [{
 },{
   "title": "Two and a Half Men",
   "imdb": "http://www.imdb.com/title/tt0369179",
-  "episodes": 238,
+  "episodes": 239,
   "episodeLength": 21,
   "stillWatching": 1,
-  'lastWatched': 'S11E14'
+  'lastWatched': 'S11E15'
 }, {
   "title": "White Collar",
   "imdb": "http://www.imdb.com/title/tt1358522/",
