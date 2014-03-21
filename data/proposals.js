86ce098b854218306ba89cf7c29912a0213f597c
Michal Budzynski <michal@virtualdesign.pl>
2014-03-21 10:18:20 +0100

    [TV Shows] IMDB Link -> IMDB ID in proposals

--- data/proposals.js
+++ data/proposals.js
@@ -5,203 +5,203 @@
 
 var toConsider = [{
   "title": "Almost Human",
-  "imdb": "http://www.imdb.com/title/tt2654580",
+  "imdb": "tt2654580",
   "episodeLength": 45,
   "stillWatching": 1,
   "votes": ["scryptonite"]
 },
 {
   "title": "Arrested development",
-  "imdb": "http://www.imdb.com/title/tt0367279/",
+  "imdb": "tt0367279",
   "episodeLength": 28,
   "stillWatching": 0,
   "votes": ["autonome"],
   'wiki' : 'Arrested_Development_(TV_series)'
 },{
   "title": "Band of Brothers",
-  "imdb": "http://www.imdb.com/title/tt0185906",
+  "imdb": "tt0185906",
   "episodeLength": 60,
   "stillWatching": 0,
   "votes": ["tobytailor"]
 }, {
   "title": "Black Mirror",
-  "imdb": "http://www.imdb.com/title/tt2085059/",
+  "imdb": "tt2085059",
   "episodeLength": 60,
   "stillWatching": 0,
   "votes": ["tizianozonta"],
   'wiki' : 'Black Mirror (TV series)'
 }, {
   "title": "Burn Notice",
-  "imdb": "http://www.imdb.com/title/tt0810788/",
+  "imdb": "tt0810788",
   "episodeLength": 60,
   "stillWatching": 0,
   "votes": ["1marc"]
 }, {
   "title": "Chuck",
-  "imdb": "http://www.imdb.com/title/tt0934814",
+  "imdb": "tt0934814",
   "episodeLength": 43,
   "stillWatching": 1,
   "votes": ["kamilogorek", "sirgregg", "scryptonite"]
 }, {
   "title": "Continuum",
-  "imdb": "http://www.imdb.com/title/tt1954347",
+  "imdb": "tt1954347",
   "episodeLength": 45,
   "stillWatching": 1,
   "votes": ["scryptonite"]
 }, {
   "title": "Community",
-  "imdb": "http://www.imdb.com/title/tt1439629/",
+  "imdb": "tt1439629",
   "episodeLength": 22,
   "stillWatching": 1,
   "votes": ["sirgregg"]
 }, {
   "title": "Cowboy Bebop",
-  "imdb": "http://www.imdb.com/title/tt0213338/",
+  "imdb": "tt0213338",
   "episodeLength": 25,
   "stillWatching": 0,
   "votes": ["datrio", "kamilogorek"]
 }, {
   "title": "Dollhouse",
-  "imdb": "http://www.imdb.com/title/tt1135300/",
+  "imdb": "tt1135300",
   "episodeLength": 45,
   "stillWatching": 0,
   "votes": ["igorw"]
 }, {
   "title": "Homeland",
-  "imdb": "http://www.imdb.com/tt1796960",
+  "imdb": "tt1796960",
   "episodeLength": 60,
   "stillWatching": 1,
   "votes": ["tobytailor", "shorang", "ryanburgess"]
 }, {
   "title": "It's Always Sunny in Philadelphia",
-  "imdb": "http://www.imdb.com/title/tt0472954/",
+  "imdb": "tt0472954",
   "episodeLength": 22,
   "stillWatching": 1,
   "votes": ["sirgregg", "ryanburgess"]
 }, {
   "title": "Mad Men",
-  "imdb": "http://www.imdb.com/title/tt0804503",
+  "imdb": "tt0804503",
   "episodeLength": 45,
   "stillWatching": 1,
   "votes": ["kamilogorek", "datrio", "ryanburgess"]
 }, {
   "title": "Modern Family",
-  "imdb": "http://www.imdb.com/title/tt1442437/",
+  "imdb": "tt1442437",
   "episodeLength": 30,
   "stillWatching": 1,
   "votes": ["afronski", "kamilogorek", "ryanburgess"]
 }, {
   "title": "My Name Is Earl",
-  "imdb": "http://www.imdb.com/title/tt0460091",
+  "imdb": "tt0460091",
   "episodeLength": 30,
   "stillWatching": 0,
   "votes": ["kamilogorek", "sirgregg"]
 }, {
   "title": "Numb3rs",
-  "imdb": "http://www.imdb.com/title/tt0433309/",
+  "imdb": "tt0433309",
   "episodeLength": 43,
   "stillWatching": 0,
   "votes": ["afronski"],
   'wiki' : 'Numbers (TV series)'
 }, {
   "title": "Oz",
-  "imdb": "http://www.imdb.com/title/tt0118421",
+  "imdb": "tt0118421",
   "episodeLength": 55,
   "stillWatching": 0,
   "votes": ["kamilogorek"]
 }, {
   "title": "Parks and Recreation",
-  "imdb": "http://www.imdb.com/title/tt1266020",
+  "imdb": "tt1266020",
   "episodeLength": 21,
   "stillWatching": 1,
   "votes": ["kamilogorek", "sirgregg", "ryanburgess"]
 }, {
   "title": "Person of Interest",
-  "imdb": "http://www.imdb.com/title/tt1839578",
+  "imdb": "tt1839578",
   "episodeLength": 43,
   "stillWatching": 0,
   "votes": ["shorang", "scryptonite"],
   'wiki' : 'Person of Interest (TV series)'
 }, {
   "title": "Sons of Anarchy",
-  "imdb": "http://www.imdb.com/title/tt1124373",
+  "imdb": "tt1124373",
   "episodeLength": 45,
   "stillWatching": 1,
   "votes": ["kamilogorek", "tobytailor","tizianozonta", "ryanburgess"]
 }, {
   "title": "Spartacus",
-  "imdb": "http://www.imdb.com/title/tt1442449",
+  "imdb": "tt1442449",
   "episodeLength": 60,
   "stillWatching": 1,
   "votes": ["kamilogorek", "datrio", "afronski", "tobytailor"]
 }, {
   "title": "The Following",
-  "imdb": "http://www.imdb.com/title/tt2071645",
+  "imdb": "tt2071645",
   "episodeLength": 43,
   "stillWatching": 0,
   "votes": ["shorang", "ryanburgess"]
 }, {
   "title": "The Newsroom",
-  "imdb": "http://www.imdb.com/tt1870479",
+  "imdb": "tt1870479",
   "episodeLength": 60,
   "stillWatching": 1,
   "votes": ["tobytailor"],
   'wiki' : 'The Newsroom (U.S. TV series)'
 }, {
   "title": "The Office",
-  "imdb": "http://www.imdb.com/title/tt0386676",
+  "imdb": "tt0386676",
   "episodeLength": 22,
   "stillWatching": 0,
   "votes": ["kamilogorek", "ryanburgess"],
   'wiki' : 'The Office (U.S. TV series)'
 }, {
   "title": "The Pacific",
-  "imdb": "http://www.imdb.com/tt0374463",
+  "imdb": "tt0374463",
   "episodeLength": 60,
   "stillWatching": 0,
   "votes": ["tobytailor"],
   'wiki' : 'The_Pacific_(TV_miniseries)'
 }, {
   "title": "The Shield",
-  "imdb": "http://www.imdb.com/tt0286486",
+  "imdb": "tt0286486",
   "episodeLength": 42,
   "stillWatching": 0,
   "votes": ["tobytailor"]
 }, {
   "title": "The Sopranos",
-  "imdb": "http://www.imdb.com/tt0141842",
+  "imdb": "tt0141842",
   "episodeLength": 55,
   "stillWatching": 0,
   "votes": ["tobytailor", "ryanburgess"]
 }, {
   "title": "The Walking Dead",
-  "imdb": "http://www.imdb.com/title/tt1520211/",
+  "imdb": "tt1520211",
   "episodeLength": 44,
   "stillWatching": 1,
   "votes": ["datrio", "afronski", "tobytailor", "shorang", "ryanburgess", "scryptonite"],
   'wiki' : 'The Walking Dead (TV series)'
 }, {
   "title": "The West Wing",
-  "imdb": "http://www.imdb.com/title/tt0200276/",
+  "imdb": "tt0200276",
   "episodeLength": 42,
   "stillWatching": 0,
   "votes": ["datrio"]
 }, {
   "title": "Warehouse 13",
-  "imdb": "http://www.imdb.com/title/tt1132290/",
+  "imdb": "tt1132290",
   "episodeLength": 45,
   "stillWatching": 0,
   "votes": ["hounddog"]
 }, {
   "title": "Weeds",
-  "imdb": "http://www.imdb.com/title/tt0439100/",
+  "imdb": "tt0439100",
   "episodeLength": 30,
   "stillWatching": 0,
   "votes": ["tizianozonta", "ryanburgess"],
   'wiki' : 'Weeds_(TV_series)'
 }, {
   "title": "Wilfred",
-  "imdb": "http://www.imdb.com/title/tt1703925",
+  "imdb": "tt1703925",
   "episodeLength": 30,
   "stillWatching": 1,
   "votes": ["kamilogorek", "sirgregg"],
