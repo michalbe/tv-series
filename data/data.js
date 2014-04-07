a064c2045580a29e106878122fa6e13da7ddcbdf
Michal Budzynski <michal@virtualdesign.pl>
2014-04-07 16:54:08 +0700

    [TV Shows] Hannibal

--- data/data.js
+++ data/data.js
@@ -192,9 +192,9 @@ module.exports = [
   {
     "title": "Hannibal",
     "imdb": "tt2243973",
-    "episodes": 17,
+    "episodes": 18,
     "stillWatching": 1,
-    "lastWatched": "S02E04"
+    "lastWatched": "S02E05"
   },
   {
     "title": "Heroes",
