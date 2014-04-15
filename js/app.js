ba5e581d8ccfd8ddd793518fe819f41cceca8015
Michal Budzynski <michal@virtualdesign.pl>
2014-04-15 12:07:00 +0100

    [TV] - :tiger: change in min2hours filter

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ var app = angular.module('tvshows', []);
 
 app.filter('min2hours', function() {
   return function(min) {
-    return ~~( min / 60) + "h " +
-    (min%60 === 0 ? '' : min%60 + "min");
+    return ~~( min / 60) + "h" +
+    (min%60 === 0 ? '' : ' ' + min%60 + "min");
   };
 });
 
