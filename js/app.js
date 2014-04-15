627c82eda18b5c3a55c32b1edde5d6373f4dfc4c
Michal Budzynski <michal@virtualdesign.pl>
2014-04-15 12:07:00 +0100

    [TV] - :dart: controllers & filters

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 var app = angular.module('tvshows', []);
+
+app.filter('min2hours', function() {
+  return function(min) {
+    return ~~( min / 60) + "h " +
+    (min%60 === 0 ? '' : min%60 + "min");
+  };
+});
+
+var shows = [{
+  "title" : "Anger Management",
+  "imdb":"tt1986770",
+  "episodes": 69,
+  "stillWatching": 1,
+  "lastWatched":"S02E59",
+  "wiki":"Anger_Management_(TV_series)",
+  "episodeLength": 21
+}];
+
+app.controller('ListController', function() {
+  this.shows = shows;
+});
