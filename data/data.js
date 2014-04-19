a66f5678b313511ca1a7075b5390606a0c0914fc
Michal Budzynski <michal@virtualdesign.pl>
2014-04-19 21:09:00 +0100

    [TV Shows] Silicon Valley

--- data/data.js
+++ data/data.js
@@ -369,8 +369,8 @@ module.exports = [
     "title": "Silicon Valley",
     "imdb": "tt2575988",
     "stillWatching": 1,
-    "episodes": 1,
-    "lastWatched": "S01E01"
+    "episodes": 2,
+    "lastWatched": "S01E02"
   },
   {
     "title": "Simpsons",
