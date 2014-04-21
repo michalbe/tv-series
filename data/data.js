69162f0544255957b1c44bfaefcdb7e065f28ff4
Michal Budzynski <michal@virtualdesign.pl>
2014-04-21 10:11:45 +0200

    [TV Shows] Blok Ekipa

--- data/data.js
+++ data/data.js
@@ -57,7 +57,7 @@ module.exports = [
   },
   {
     "title": "Blok Ekipa",
-    "episodes": 19,
+    "episodes": 20,
     "poster": "http://i1.ytimg.com/vi/yk_4kQjgBJw/maxresdefault.jpg",
     "episodeLength": 5,
     "stillWatching": 1
