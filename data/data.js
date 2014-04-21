a794a49227e8e08ce8be2f15125bf169eaa026e4
Michal Budzynski <michal@virtualdesign.pl>
2014-04-22 00:23:01 +0200

    [TV SHOWS] Family guy

--- data/data.js
+++ data/data.js
@@ -146,9 +146,9 @@ module.exports = [
   {
     "title": "Family Guy",
     "imdb": "tt0182576",
-    "episodes": 226,
+    "episodes": 227,
     "stillWatching": 1,
-    "lastWatched": "S12E16"
+    "lastWatched": "S12E17"
   },
   {
     "title": "FlashForward",
