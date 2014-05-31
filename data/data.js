7c18a86cb5ba20eeb2398fb6a69ae79e7b5623b2
Michal Budzynski <michal@virtualdesign.pl>
2014-05-31 20:25:31 +0200

    B99

--- data/data.js
+++ data/data.js
@@ -77,8 +77,8 @@ module.exports = [
     "title": "Brooklyn Nine-Nine",
     "imdb": "tt2467372",
     "stillWatching": 1,
-    "episodes": 4,
-    "lastWatched": "S01E04"
+    "episodes": 5,
+    "lastWatched": "S01E05"
   },
   {
     "title": "Boardwalk Empire",
