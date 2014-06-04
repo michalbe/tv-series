8a0ed3e309692c2739ef0b3a4060b8fbb839b1dd
Michal Budzynski <michal@virtualdesign.pl>
2014-06-04 15:31:29 +0800

    [TV] - Episode number fix

--- data/data.js
+++ data/data.js
@@ -358,9 +358,9 @@ module.exports = [
   {
     "title": "Rick and Morty",
     "imdb": "tt2861424",
-    "episodes": 3,
+    "episodes": 2,
     "stillWatching": 1,
-    "lastWatched": "S01E03"
+    "lastWatched": "S01E02"
   },
   {
     "title": "Scrubs",
