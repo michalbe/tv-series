41dcee1e58a214f5456941644b097aedd6e9fc1e
Michal Budzynski <michal@virtualdesign.pl>
2014-06-06 06:00:44 +0800

    [TV] - R&M

--- data/data.js
+++ data/data.js
@@ -359,9 +359,9 @@ module.exports = [
   {
     "title": "Rick and Morty",
     "imdb": "tt2861424",
-    "episodes": 10,
+    "episodes": 11,
     "stillWatching": 1,
-    "lastWatched": "S01E10"
+    "lastWatched": "S01E11"
   },
   {
     "title": "Scrubs",
