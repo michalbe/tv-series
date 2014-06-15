b7c713de1de9ff5d07ab6dd7642f272e5fe7d2f7
Michal Budzynski <michal@virtualdesign.pl>
2014-06-15 18:22:00 +0100

    [TV] - mf

--- data/data.js
+++ data/data.js
@@ -332,8 +332,8 @@ module.exports = [
   {
     "title": "Modern Family",
     "imdb": "tt1442437",
-    "lastWatched": "S03E21",
-    "episodes": 69,
+    "lastWatched": "S03E22",
+    "episodes": 70,
     "stillWatching": 1,
     "oldvotes": [
       "afronski",
