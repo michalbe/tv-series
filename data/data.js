d3e9a741e3f6882289cf953e61046e1e1546a9b6
Michal Budzynski <michal@virtualdesign.pl>
2014-07-02 11:36:49 +0200

    :tv: - Anger Management

--- data/data.js
+++ data/data.js
@@ -28,9 +28,9 @@ module.exports = [
   {
     "title": "Anger Management",
     "imdb": "tt1986770",
-    "episodes": 60,
+    "episodes": 61,
     "stillWatching": 1,
-    "lastWatched": "S02E50",
+    "lastWatched": "S02E51",
     "wiki": "Anger_Management_(TV_series)"
   },
   {
