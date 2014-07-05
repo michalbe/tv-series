408d1dcc0bb2cc1ee2d9c91e116e72c8e6beb8c5
Michal Budzynski <michal@virtualdesign.pl>
2014-07-05 15:39:07 +0200

    :tv: - Legit

--- data/data.js
+++ data/data.js
@@ -280,9 +280,9 @@ module.exports = [
   {
     "title": "Legit",
     "imdb": "tt2400391",
-    "episodes": 1,
+    "episodes": 3,
     "stillWatching": 1,
-    "lastWatched": "S01E01",
+    "lastWatched": "S01E03",
     "wiki": "Legit_(2013_TV_series)"
   },
   {
