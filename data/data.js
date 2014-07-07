ce237dd6ee27fc426f3e2d647ebd6550892e97ea
Michal Budzynski <michal@virtualdesign.pl>
2014-07-07 21:22:33 +0200

    [TV] - :fried_shrimp: Anger management

--- data/data.js
+++ data/data.js
@@ -28,9 +28,9 @@ module.exports = [
   {
     "title": "Anger Management",
     "imdb": "tt1986770",
-    "episodes": 69,
+    "episodes": 70,
     "stillWatching": 1,
-    "lastWatched": "S02E59",
+    "lastWatched": "S02E60",
     "wiki": "Anger_Management_(TV_series)"
   },
   {
