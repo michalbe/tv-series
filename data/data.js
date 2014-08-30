3e7fbc6c4f4fda7c99e3ee400e5093a82b59d3ff
Michal Budzynski <michal@virtualdesign.pl>
2014-08-30 04:37:41 +0200

    [TV] - :alien: Anger Management

--- data/data.js
+++ data/data.js
@@ -28,9 +28,9 @@ module.exports = [
   {
     "title": "Anger Management",
     "imdb": "tt1986770",
-    "episodes": 72,
+    "episodes": 73,
     "stillWatching": 1,
-    "lastWatched": "S02E62",
+    "lastWatched": "S02E63",
     "wiki": "Anger_Management_(TV_series)"
   },
   {
