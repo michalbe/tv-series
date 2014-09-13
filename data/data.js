2c50d7083489e39056ac64170dff329f573245e9
Michał Budzyński <michal@virtualdesign.pl>
2014-09-13 16:04:07 +0200

    [TV] - :dash: Franklin & Bash

--- data/data.js
+++ data/data.js
@@ -174,9 +174,9 @@ module.exports = [
   {
     "title": "Franklin & Bash",
     "imdb": "tt1600199",
-    "episodes": 30,
+    "episodes": 31,
     "stillWatching": 1,
-    "lastWatched": "S3E10",
+    "lastWatched": "S04E01",
     "wiki": "Franklin_%26_Bash"
   },
   {
