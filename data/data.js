c72b3c083d5e850646a6685fae033f23514eb0b3
Michał Budzyński <michal@virtualdesign.pl>
2014-09-14 09:30:47 +0200

    [TV] - :coffee: sopranos

--- data/data.js
+++ data/data.js
@@ -459,9 +459,9 @@ module.exports = [
   {
     "title": "The Sopranos",
     "imdb": "tt0141842",
-    "episodes": 65,
+    "episodes": 66,
     "stillWatching": 1,
-    "lastWatched": "S05E13"
+    "lastWatched": "S06E01"
   },
   {
     "title": "True Detective",
