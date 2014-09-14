02cdc18c37f4fcee22567deb9e0562e955ca2dda
Michał Budzyński <michal@virtualdesign.pl>
2014-09-14 09:31:33 +0200

    [TV] - :ski: F

--- data/data.js
+++ data/data.js
@@ -174,9 +174,9 @@ module.exports = [
   {
     "title": "Franklin & Bash",
     "imdb": "tt1600199",
-    "episodes": 31,
+    "episodes": 32,
     "stillWatching": 1,
-    "lastWatched": "S04E01",
+    "lastWatched": "S04E02",
     "wiki": "Franklin_%26_Bash"
   },
   {
