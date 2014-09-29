38c281b133adb53b51cdd3dadb6f2a290f360687
Michał Budzyński <michal@virtualdesign.pl>
2014-09-29 09:54:42 +0200

    [TV] - :suspension_railway: Sopranos

--- data/data.js
+++ data/data.js
@@ -459,9 +459,9 @@ module.exports = [
   {
     "title": "The Sopranos",
     "imdb": "tt0141842",
-    "episodes": 72,
+    "episodes": 73,
     "stillWatching": 1,
-    "lastWatched": "S06E07"
+    "lastWatched": "S06E08"
   },
   {
     "title": "True Detective",
