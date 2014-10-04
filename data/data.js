1faddaa1d3046f3cbad8af183cd6d99766f8a907
Michał Budzyński <michal@virtualdesign.pl>
2014-10-04 19:10:05 +0200

    [TV] - :camera: Sopranos

--- data/data.js
+++ data/data.js
@@ -459,9 +459,9 @@ module.exports = [
   {
     "title": "The Sopranos",
     "imdb": "tt0141842",
-    "episodes": 83,
+    "episodes": 84,
     "stillWatching": 1,
-    "lastWatched": "S06E18"
+    "lastWatched": "S06E19"
   },
   {
     "title": "True Detective",
