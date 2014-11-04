f511894a9493bbf2ca2fcd77ead61480cddcc4e0
Michał Budzyński <michal@virtualdesign.pl>
2014-11-05 02:26:36 +0400

    [TV] - :tongue: Brooklyn 9-9

--- data/data.js
+++ data/data.js
@@ -91,8 +91,8 @@ module.exports = [
     "title": "Brooklyn Nine-Nine",
     "imdb": "tt2467372",
     "stillWatching": 1,
-    "episodes": 26,
-    "lastWatched": "S02E04"
+    "episodes": 27,
+    "lastWatched": "S02E05"
   },
   {
     "title": "CSI: Crime Scene Investigation",
