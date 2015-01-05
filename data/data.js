6fcf08a8c8fd70fdf9b1af7fdce3d4b471df7452
Michał Budzyński <michal@virtualdesign.pl>
2015-01-05 01:01:00 +0100

    [TV] - :bust_in_silhouette: Arrested Development

--- data/data.js
+++ data/data.js
@@ -36,9 +36,9 @@ module.exports = [
   {
     "title": "Arrested Development",
     "imdb": "tt0367279",
-    "episodes": 51,
+    "episodes": 53,
     "stillWatching": 1,
-    "lastWatched": "S03E11",
+    "lastWatched": "S03E13",
     "wiki": "Arrested_Development_(TV_series)"
   },
   {
