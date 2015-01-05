651a98e90b39e9d0ed8b21006d77884c0cde3699
Michał Budzyński <michal@virtualdesign.pl>
2015-01-05 01:01:00 +0100

    [TV] - :couple: TBBT

--- data/data.js
+++ data/data.js
@@ -57,9 +57,9 @@ module.exports = [
   {
     "title": "Big Bang Theory",
     "imdb": "tt0898266",
-    "episodes": 170,
+    "episodes": 171,
     "stillWatching": 1,
-    "lastWatched": "S08E11",
+    "lastWatched": "S08E12",
     "wiki": "The_Big_Bang_Theory"
   },
   {
