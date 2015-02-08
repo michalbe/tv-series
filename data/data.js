beba980a31bf6ace7fe41c3e238dd2496da9be06
Michał Budzyński <michal@virtualdesign.pl>
2015-02-08 01:01:00 +0100

    [TV] - :video_game: blok ekipa

--- data/data.js
+++ data/data.js
@@ -64,7 +64,7 @@ module.exports = [
   },
   {
     "title": "Blok Ekipa",
-    "episodes": 41,
+    "episodes": 42,
     "poster": "http://i1.ytimg.com/vi/yk_4kQjgBJw/maxresdefault.jpg",
     "episodeLength": 5,
     "stillWatching": 1
