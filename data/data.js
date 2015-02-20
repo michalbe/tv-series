1882ea3b4945eb2455befc6d59330d341f8d1238
Michał Budzyński <michal@virtualdesign.pl>
2015-02-20 17:04:24 +0100

    [TV] - :light_rail: family guy

--- data/data.js
+++ data/data.js
@@ -174,10 +174,10 @@ module.exports = [
   {
     "title": "Family Guy",
     "imdb": "tt0182576",
-    "episodes": 240,
+    "episodes": 241,
     "stillWatching": 1,
     "episodeLength": 22,
-    "lastWatched": "S13E09"
+    "lastWatched": "S13E10"
   },
   {
     "title": "Fargo",
