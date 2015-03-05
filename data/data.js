7e4278a062940ca670911b3e4ea19c063b0762f4
Michał Budzyński <michal@virtualdesign.pl>
2015-03-05 23:55:51 +0100

    [TV] - :yellow_heart: house of cards

--- data/data.js
+++ data/data.js
@@ -265,9 +265,9 @@ module.exports = [
   {
     "title": "House of Cards",
     "imdb": "tt1856010",
-    "episodes": 26,
+    "episodes": 28,
     "stillWatching": 1,
-    "lastWatched": "S03E00",
+    "lastWatched": "S03E02",
     "wiki": "House_of_Cards_(U.S._TV_series)"
   },
   {
