4e4705e2306502e168694a88ede04a8bdb62e257
Michał Budzyński <michal@virtualdesign.pl>
2015-04-26 01:01:00 +0100

    [TV] - :purple_heart:

--- data/data.js
+++ data/data.js
@@ -396,8 +396,8 @@ module.exports = [
   {
     "title": "Modern Family",
     "imdb": "tt1442437",
-    "lastWatched": "S06E19",
-    "episodes": 139,
+    "lastWatched": "S06E20",
+    "episodes": 140,
     "stillWatching": 1,
     "oldvotes": [
       "afronski",
