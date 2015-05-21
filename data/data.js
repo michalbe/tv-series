78b15d42aa209d71aef6e68a102a230e54dec1cb
Michał Budzyński <michal@virtualdesign.pl>
2015-05-21 18:16:48 +0200

    silicon vally

--- data/data.js
+++ data/data.js
@@ -493,8 +493,8 @@ module.exports = [
     "title": "Silicon Valley",
     "imdb": "tt2575988",
     "stillWatching": 1,
-    "episodes": 14,
-    "lastWatched": "S02E06"
+    "episodes": 15,
+    "lastWatched": "S02E07"
   },
   {
     "title": "Simpsons",
