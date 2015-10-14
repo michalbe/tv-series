5e1ec5bd543aa4416a89a21927ce830145c66827
Michał Budzyński <michal@virtualdesign.pl>
2015-10-14 01:01:00 +0100

    [TV] - :baseball: Limitless

--- data/data.js
+++ data/data.js
@@ -372,9 +372,9 @@ module.exports = [
   {
     "title": "Limitless",
     "imdb": "tt4422836",
-    "episodes": 3,
+    "episodes": 4,
     "stillWatching": 1,
-    "lastWatched": "S01E03"
+    "lastWatched": "S01E04"
   },
   {
     "title": "Lost",
