75a8e3799584e29653b2c3ee5052c62c07f71417
Michał Budzyński <michal@virtualdesign.pl>
2015-12-27 03:05:14 +0100

    [TV] - :earth_asia: big bang theory

--- data/data.js
+++ data/data.js
@@ -57,9 +57,9 @@ module.exports = [
   {
     "title": "Big Bang Theory",
     "imdb": "tt0898266",
-    "episodes": 193,
+    "episodes": 194,
     "stillWatching": 1,
-    "lastWatched": "S09E10",
+    "lastWatched": "S09E11",
     "wiki": "The_Big_Bang_Theory"
   },
   {
