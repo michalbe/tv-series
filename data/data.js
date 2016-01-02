524900ce0c48af2a5cb57c9eebcc43500af116ab
Michał Budzyński <michal@virtualdesign.pl>
2016-01-02 16:30:00 +0100

    [TV] - :moyai: Brooklyn 99

--- data/data.js
+++ data/data.js
@@ -105,8 +105,8 @@ module.exports = [
     "title": "Brooklyn Nine-Nine",
     "imdb": "tt2467372",
     "stillWatching": 1,
-    "episodes": 55,
-    "lastWatched": "S03E10"
+    "episodes": 54,
+    "lastWatched": "S03E09"
   },
   {
     "title": "CSI: Crime Scene Investigation",
