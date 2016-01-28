063595a53f1fe2fff97b8d920ffada83b41de89a
Michał Budzyński <michal@virtualdesign.pl>
2016-01-28 03:50:00 +0100

    [TV] - :e-mail: Modern family

--- data/data.js
+++ data/data.js
@@ -425,8 +425,8 @@ module.exports = [
   {
     "title": "Modern Family",
     "imdb": "tt1442437",
-    "lastWatched": "S07E03",
-    "episodes": 147,
+    "lastWatched": "S07E04",
+    "episodes": 148,
     "stillWatching": 1,
     "oldvotes": [
       "afronski",
