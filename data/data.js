354e66ac7ae4f9074be801703516f994d0d86820
Michał Budzyński <michal@virtualdesign.pl>
2016-02-01 03:54:00 +0100

    [TV] - :confused: Modern family

--- data/data.js
+++ data/data.js
@@ -425,8 +425,8 @@ module.exports = [
   {
     "title": "Modern Family",
     "imdb": "tt1442437",
-    "lastWatched": "S07E07",
-    "episodes": 151,
+    "lastWatched": "S07E08",
+    "episodes": 152,
     "stillWatching": 1,
     "oldvotes": [
       "afronski",
