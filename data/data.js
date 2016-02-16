a438719eefb878d979cdcc94bd8d61d3524a27da
Michał Budzyński <michal@virtualdesign.pl>
2016-02-16 04:59:27 +0100

    [TV] - :baby: limitless

--- data/data.js
+++ data/data.js
@@ -372,9 +372,9 @@ module.exports = [
   {
     "title": "Limitless",
     "imdb": "tt4422836",
-    "episodes": 13,
+    "episodes": 14,
     "stillWatching": 1,
-    "lastWatched": "S01E13"
+    "lastWatched": "S01E14"
   },
   {
     "title": "Lost",
