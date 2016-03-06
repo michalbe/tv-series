3ef0ec1fd7d7acdf033bc38e58f6c38cbec5fe95
Michał Budzyński <michal@virtualdesign.pl>
2016-03-06 08:23:00 +0100

    [TV] - :pill: House of Cards

--- data/data.js
+++ data/data.js
@@ -294,9 +294,9 @@ module.exports = [
   {
     "title": "House of Cards",
     "imdb": "tt1856010",
-    "episodes": 39,
+    "episodes": 40,
     "stillWatching": 1,
-    "lastWatched": "S03E13",
+    "lastWatched": "S04E01",
     "wiki": "House_of_Cards_(U.S._TV_series)"
   },
   {
