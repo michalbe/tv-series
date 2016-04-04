59e23f2496c1cb03e588c074d18b2f9ba8bea7ec
Michał Budzyński <michal@virtualdesign.pl>
2016-04-04 03:18:00 +0100

    [TV] - :calendar: suits

--- data/data.js
+++ data/data.js
@@ -578,9 +578,9 @@ module.exports = [
   {
     "title": "Suits",
     "imdb": "tt1632701",
-    "episodes": 75,
+    "episodes": 76,
     "stillWatching": 1,
-    "lastWatched": "S05E15",
+    "lastWatched": "S05E16",
     "wiki": "Suits_(TV_series)"
   },
   {
