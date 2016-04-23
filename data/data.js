11debd9cb1d017b2df7f57a7d3aa3baf62c7e96f
Michał Budzyński <michal@virtualdesign.pl>
2016-04-23 17:17:00 +0100

    [TV] - :earth_asia: Legends of tomorrow

--- data/data.js
+++ data/data.js
@@ -366,9 +366,9 @@ module.exports = [
   {
     "title": "Legends of Tomorrow",
     "imdb": "tt4532368",
-    "episodes": 11,
+    "episodes": 12,
     "stillWatching": 1,
-    "lastWatched": "S01E11"
+    "lastWatched": "S01E12"
   },
   {
     "title": "Legit",
