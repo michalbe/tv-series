a4e7035835e0462721fd3279aeda67692518632c
Michał Budzyński <michal@virtualdesign.pl>
2016-05-03 01:06:41 +0200

    [TV] - :ramen: Clone Wars

--- data/data.js
+++ data/data.js
@@ -570,9 +570,9 @@ module.exports = [
   {
     "title": "Star Wars: The Clone Wars",
     "imdb": "tt0458290",
-    "episodes": 11,
+    "episodes": 16,
     "stillWatching": 1,
-    "lastWatched": "S01E11",
+    "lastWatched": "S01E16",
     "wiki": "Star_Wars:_The_Clone_Wars_(2008_TV_series)"
   },
   {
