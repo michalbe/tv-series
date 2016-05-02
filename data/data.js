4a0a7c9624a2a1ab83b91a08eeda5f8d37337b72
Michał Budzyński <michal@virtualdesign.pl>
2016-05-02 06:23:31 +0200

    [TV] - :gift_heart: Clone Wars

--- data/data.js
+++ data/data.js
@@ -570,9 +570,9 @@ module.exports = [
   {
     "title": "Star Wars: The Clone Wars",
     "imdb": "tt0458290",
-    "episodes": 5,
+    "episodes": 7,
     "stillWatching": 1,
-    "lastWatched": "S01E05",
+    "lastWatched": "S01E07",
     "wiki": "Star_Wars:_The_Clone_Wars_(2008_TV_series)"
   },
   {
