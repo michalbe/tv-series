576090fba46472524fbfdc2d8be5ce0521fe8b60
Michał Budzyński <michal@virtualdesign.pl>
2016-07-31 17:55:00 +0100

    [TV] - :waning_crescent_moon: Archer

--- data/data.js
+++ data/data.js
@@ -50,9 +50,9 @@ module.exports = [
   {
     "title": "Archer",
     "imdb": "tt1486217",
-    "episodes": 0,
+    "episodes": 4,
     "stillWatching": 1,
-    "lastWatched": "S01E00"
+    "lastWatched": "S01E04"
   },
   {
     "title": "Arrested Development",
