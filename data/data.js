fcda642162a182a7f27e7b1644098d641ed95b22
Michał Budzyński <michal@virtualdesign.pl>
2016-08-15 01:05:00 +0100

    [TV] - :black_nib: Gravity falls

--- data/data.js
+++ data/data.js
@@ -267,9 +267,9 @@ module.exports = [
   {
     "title": "Gravity Falls",
     "imdb": "tt1865718",
-    "episodes": 11,
+    "episodes": 17,
     "stillWatching": 1,
-    "lastWatched": "S01E11"
+    "lastWatched": "S01E17"
   },
   {
     "title": "Hannibal",
